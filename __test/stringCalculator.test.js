39632ad6595a3e2dd830462493843a7e70d3a645
rahul4code <you@example.com>
2024-07-09 01:36:48 +0530

    test case with single number

--- __test/stringCalculator.test.js
+++ __test/stringCalculator.test.js
@@ -3,3 +3,7 @@ const StringCalculator = require("../src/stringCalculator");
 test("returns 0 for an empty string", () => {
   expect(StringCalculator.add("")).toBe(0);
 });
+
+test("returns the number when only one number is provided", () => {
+  expect(StringCalculator.add("1")).toBe(1);
+});
