78c771bc357458bf259743ee5feda95d4af68896
rahul4code <you@example.com>
2024-07-09 01:42:30 +0530

    handle different delimiters

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -3,7 +3,14 @@ class StringCalculator {
     if (numbers === "") {
       return 0;
     }
-    const numArray = numbers.split(/[\n,]/);
+    let delimiter = /[\n,]/;
+    if (numbers.startsWith("//")) {
+      const parts = numbers.split("\n");
+      delimiter = new RegExp(`[${parts[0].charAt(2)}]`);
+      numbers = parts[1];
+    }
+
+    const numArray = numbers.split(delimiter);
     return numArray.reduce((sum, num) => sum + parseInt(num), 0);
   }
 }
